e7e5a1f8b62beb3956989acd9cb6b9bfaaed562e
JuanPi420 <jpmarcoschapa@gmail.com>
2025-03-19 10:22:07 -0600

    Update Weapon.java
    New weapons for secret character/ Spear/ balance changes

--- app/src/main/java/org/game/Weapon.java
+++ app/src/main/java/org/game/Weapon.java
@@ -19,71 +19,71 @@ public class Weapon {
             case TWO_HANDED_AXE:
                 attackMessage = "Swoosh Choosh Chop!";
                 minDamage = 50;
-                maxDamage = gen.ints(51, 60).findFirst().getAsInt();
-                critChance = 0.15;
-                missChance = 0.0;
+                maxDamage = gen.ints(54, 60).findFirst().getAsInt();
+                critChance = 0.25;
+                missChance = 0.15;
                 break;
             case TWO_HANDED_MACE:
                 attackMessage = "MEGA BONK";
-                minDamage = 45;
-                maxDamage = gen.ints(46, 60).findFirst().getAsInt();
-                critChance = 0.15;
-                missChance = 0.0;
+                minDamage = 37;
+                maxDamage = gen.ints(44, 60).findFirst().getAsInt();
+                critChance = 0.30;
+                missChance = 0.20;
                 break;
             case ONE_HANDED_SWORD:
-                attackMessage = "-Generic- CUT";
-                minDamage = 30;
-                maxDamage = gen.ints(31, 50).findFirst().getAsInt();
+                attackMessage = "-Generic- SLASHHHH";
+                minDamage = 32;
+                maxDamage = gen.ints(40, 50).findFirst().getAsInt();
                 critChance = 0.25;
                 missChance = 0.10;
                 break;
             case ONE_HANDED_AXE:
                 attackMessage = "Ching Ching Chop";
-                minDamage = 30;
-                maxDamage = gen.ints(31, 50).findFirst().getAsInt();
-                critChance = 0.25;
-                missChance = 0.10;
+                minDamage = 28;
+                maxDamage = gen.ints(33, 50).findFirst().getAsInt();
+                critChance = 0.35;
+                missChance = 0.14;
                 break;
             case DAGGER:
                 attackMessage = "Shing shing... SHING";
-                minDamage = 23;
-                maxDamage = gen.ints(24, 45).findFirst().getAsInt();
-                critChance = 0.40;
-                missChance = 0.10;
+                minDamage = 24;
+                maxDamage = gen.ints(40, 52).findFirst().getAsInt();
+                critChance = 0.68;
+                missChance = 0.20;
                 break;
             case BOW:
                 attackMessage = "FIIIUUUuuummmm *TING*";
-                minDamage = 30;
-                maxDamage = gen.ints(31, 60).findFirst().getAsInt();
-                critChance = 0.40;
-                missChance = 0.20;
+                minDamage = 35;
+                maxDamage = gen.ints(37, 55).findFirst().getAsInt();
+                critChance = 0.50;
+                missChance = 0.30;
                 break;
             case STAFF:
                 attackMessage = "MAGICAL NOISES";
-                minDamage = 30;
-                maxDamage = gen.ints(31, 60).findFirst().getAsInt();
-                critChance = 0.15;
-                missChance = 0.15;
+                minDamage = 5;
+                maxDamage = gen.ints(30, 200).findFirst().getAsInt();
+                critChance = 0.30;
+                missChance = 0.10;
                 break;
             case WAND:
                 attackMessage = "Powerful spell in an ancient language is casted!";
-                minDamage = 23;
-                maxDamage = gen.ints(24, 50).findFirst().getAsInt();
-                critChance = 0.25;
-                missChance = 0.2;
+                minDamage = 31;
+                maxDamage = gen.ints(31, 40).findFirst().getAsInt();
+                critChance = 0.10;
+                missChance = 0.10;
                 break;
             case GUN:
                 attackMessage = "Welcome to the land of freedom (Eagle noises)";
                 minDamage = 699990;
                 maxDamage = gen.ints(699991, 100000000).findFirst().getAsInt();
-                critChance = 0.9;
-                missChance = 0.0;
+                critChance = 0.90;
+                missChance = 0.83;
                 break;
             case LEGENDARY_ULTRA_POWERFUL_MIGHTY_SWORD:
                 attackMessage = "GET OUT!!!!!!!!11!!111! (3 fire emojis)";
                 minDamage = 2;
                 maxDamage = gen.ints(3, 4).findFirst().getAsInt();
-                critChance = 0.1;
+                critChance = 0.10;
                 missChance = 0.0;
                 break;
            case JOLLY_TEETHBREAKER:
@@ -92,6 +92,41 @@ public class Weapon {
                 maxDamage = gen.ints(41, 80).findFirst().getAsInt();
                 critChance = 0.5;
                 missChance = 0.5;
+                break;
+           case SPEAR:
+                attackMessage = "Bazinga!!!!!";
+                minDamage = 18;
+                 maxDamage = gen.ints(63, 65).findFirst().getAsInt();
+                critChance = 0.43;
+                missChance = 0.20;
+                break;
+           case EIGON:
+                attackMessage = "MORGANA- Wow, looking cool Joker!";
+                minDamage = 30;
+                 maxDamage = gen.ints(60, 70).findFirst().getAsInt();
+                critChance = 0.33;
+                missChance = 0.20;
+                break;
+            case MYRIAD_TRUTHS:
+                attackMessage = "BY THE MYRIAD TRUTHS!!!";
+                minDamage = 600;
+                maxDamage = gen.ints(1000, 100000).findFirst().getAsInt();
+                critChance = 0.33;
+                missChance = 0.20;
+                break;
+            case GARU :
+                attackMessage = "MORGANA- That is incredible, Joker!";
+                minDamage = 40;
+                maxDamage = gen.ints(50, 55).findFirst().getAsInt();
+                critChance = 0.33;
+                missChance = 0.37;
+                break;
+            case AGI :
+                attackMessage = "ANN- Carmen, take em' down!";
+                minDamage = 10;
+                maxDamage = gen.ints(62, 80).findFirst().getAsInt();
+                critChance = 0.62;
+                missChance = 0.42;
                 break;
             }
 
@@ -109,5 +144,11 @@ public class Weapon {
         GUN,
         LEGENDARY_ULTRA_POWERFUL_MIGHTY_SWORD,
         JOLLY_TEETHBREAKER,
+        SPEAR,
+        EIGON,
+        MYRIAD_TRUTHS,
+        GARU,
+        AGI,
+
     }
 }
